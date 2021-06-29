01a4d5b170b038266e6597b1a650a732bf57be9f
Abhishek Kumar <Abhiak49kumar@gmail.com>
2021-06-29 14:40:03 +0530

    v1.0

--- data/init.js
+++ data/init.js
@@ -4,10 +4,10 @@ require( '../models/User' );
 require( '../models/Sessions' );
 require( '../models/Doodle' );
 
-const {NODE_ENV,DB_HOST,DB_NAME} = process.env;
+const {NODE_ENV,DB_USER,DB_PASSWORD,DB_HOST,DB_NAME} = process.env;
 
-const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` : '';
-//const connectionStr = `mongodb://${DB_HOST}/${DB_NAME}`;
+const connectionStr = NODE_ENV === 'development' ? `mongodb://${DB_HOST}/${DB_NAME}` : `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
 console.log(connectionStr);
 console.log(`Connecting to database ${DB_NAME}`);
 
@@ -21,7 +21,7 @@ mongoose.connection.on( 'open', () => {
     console.log( `Connected to database ${DB_NAME}` )
 } );
 
-mongoose.connection.on( 'error', () => {
+mongoose.connection.on( 'error', (err) => {
     console.error( `Could not connect to database ${DB_NAME}, error = `,  err.message );
     process.exit( 1 );
 } )
